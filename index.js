3645bc17cc14ad8be7e4381fc38e51c983ba1a5d
wikiyodo <wikiyodo@gmail.com>
2023-11-20 01:54:09 +0100

    use heroku port

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ function generateHistogramData(numBins, minValue, maxValue) {
   return { values: histogramValues };
 }
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(
+    `Server is running at http://localhost:${process.env.PORT || port}`
+  );
 });
